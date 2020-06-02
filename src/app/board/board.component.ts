511197529e884f1fbe7ccbecab27e43e523aaa17
Jay Harris <jay.harris@outlook.co.nz>
2020-06-02 17:44:40 +1000

    Flippy cards

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../game';
+import { Card } from '../card';
 
 @Component({
     selector: 'app-board',
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { Card } from '../card';
 
 @Component({
     selector: 'app-card',
     templateUrl: './card.component.html',
-    styleUrls: ['./card.component.css']
+    styleUrls: ['./card.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CardComponent implements OnInit {
     @Input()
-    card: Card = {
-        value: '',
-        faceUp: false
-    }
+    card: Card;
 
     constructor() { }
 
     ngOnInit() {
         console.log("init")
+    }
+
+    onClick() {
+        this.card = {
+            ...this.card,
+            faceUp: !this.card.faceUp
+        }
+        console.log('Clicked?')
     }
 }
