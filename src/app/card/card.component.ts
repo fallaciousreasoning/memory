9f15114fdec4a13efaed04c268585e877ea7893d
Jay Harris <jay.harris@outlook.co.nz>
2020-06-02 16:47:36 +1000

    Begins work on cards

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Card } from '../card';
+
+@Component({
+    selector: 'app-card',
+    templateUrl: './card.component.html',
+    styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+    card: Card = {
+        value: ''
+    }
+
+    constructor() {}
+
+    ngOnInit() {
+
+    }
+}
